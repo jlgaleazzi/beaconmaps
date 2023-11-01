e58ad9cb4cab6364ccafba9544bffc9829cbe338
Juan Galeazzi <jgaleazzi@gmail.com>
2023-11-01 18:09:49 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+function App() {
+
+
+  return (
+    <>
+      <MapContainer center={[51.505, -0.09]} zoom={13} >
+       <TileLayer
+       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+       />
+    </MapContainer>
+    </>
+  )
+}
+
+export default App
