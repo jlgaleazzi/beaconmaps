0dd275cd18c1c8bc3a7b2d12d79bd6f2b6272abe
Juan Galeazzi <jgaleazzi@gmail.com>
2023-11-02 19:58:08 -0500

    added transitions

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,45 @@
 
 import "leaflet/dist/leaflet.css";
-import {styled, useTheme, theme, CSSObject } from '@mui/material/styles';
+import {styled, useTheme, ThemeWithPropsheme, CSSObject } from '@mui/material/styles';
 import MuiAppBar, {AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { styled } from "@mui/material";
 
+const drawerWidth = 240;
 
 interface AppBarProps extends MuiAppBarProps {
   open?: boolean;
 }
+
+const openedMixin = (theme:Theme): CSSObject => ({
+  width: drawerWidth,
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflow: 'hidden',
+})
+
+const closedMixin = (theme: Theme): CSSObject => ({
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: 'hidden',
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up('sm')]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
 
 const AppBar = styled(MuiAppBar , {
   shouldForwardProp: (prop) => prop !== 'open',})
@@ -18,9 +49,32 @@ const AppBar = styled(MuiAppBar , {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
-    
+    ...(open && {
+      marginLeft: drawerWidth,
+      width: `cals(100% - ${drawerWidth}px)`,
+      transition: theme.transitions.create(['width','margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    }),
+  }));
 
-  }))
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+    boxSizing: 'border-box',
+    ...(open && {
+      ...openedMixin(theme),
+      '& .MuiDrawer-paper': openedMixin(theme),
+    }),
+    ...(!open && {
+      ...closedMixin(theme),
+      '& .MuiDrawer-paper': closedMixin(theme),
+    }),
+  }),
+);
 
 function App() {
   return (
