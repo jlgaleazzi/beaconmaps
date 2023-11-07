968f304d47740a7b41b07b416c60ce3fd8c27baa
Juan Galeazzi <jgaleazzi@gmail.com>
2023-11-06 22:02:42 -0600

    added drawer

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,14 @@ import "leaflet/dist/leaflet.css";
 import  {useState} from 'react';
 import {styled, useTheme, Theme, CSSObject } from '@mui/material/styles';
 import MuiAppBar, {AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
-import MuiDrawer from '@mui/material/Drawer';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import MenuIcon from '@mui/icons-material/Menu'
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { CssBaseline, IconButton, Toolbar, Typography } from "@mui/material";
 
 const drawerWidth = 240;
 
@@ -36,55 +42,102 @@ const closedMixin = (theme: Theme): CSSObject => ({
 const DrawerHeader = styled('div')(({ theme }) => ({
   display: 'flex',
   alignItems: 'center',
-  justifyContent: 'flex-end',
   padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
   ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
 }));
 
 
 const AppBar = styled(MuiAppBar , {
-  shouldForwardProp: (prop) => prop !== 'open',})
-  <AppBarProps>(({theme, open}) => ({
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create([`width`,`margin`],{
+  shouldForwardProp: (prop) => prop !== 'open',
+  })<AppBarProps>(({theme, open}) => ({
+    transition: theme.transitions.create([`margin`,`width`],{
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
     ...(open && {
-      marginLeft: drawerWidth,
-      width: `cals(100% - ${drawerWidth}px)`,
-      transition: theme.transitions.create(['width','margin'], {
-        easing: theme.transitions.easing.sharp,
+       width: `cals(100% - ${drawerWidth}px)`,
+       marginLeft: `${drawerWidth}px`, 
+      transition: theme.transitions.create(['margin','width'], {
+        easing: theme.transitions.easing.easeOut,
         duration: theme.transitions.duration.enteringScreen,
       }),
     }),
   }));
 
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme, open }) => ({
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: 'nowrap',
-    boxSizing: 'border-box',
-    ...(open && {
-      ...openedMixin(theme),
-      '& .MuiDrawer-paper': openedMixin(theme),
-    }),
-    ...(!open && {
-      ...closedMixin(theme),
-      '& .MuiDrawer-paper': closedMixin(theme),
-    }),
-  }),
-);
+
+
+// const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+//   ({ theme, open }) => ({
+//     width: drawerWidth,
+//     flexShrink: 0,
+//     whiteSpace: 'nowrap',
+//     boxSizing: 'border-box',
+//     ...(open && {
+//       ...openedMixin(theme),
+//       '& .MuiDrawer-paper': openedMixin(theme),
+//     }),
+//     ...(!open && {
+//       ...closedMixin(theme),
+//       '& .MuiDrawer-paper': closedMixin(theme),
+//     }),
+//   }),
+// );
 
 function App() {
 
 const theme = useTheme();
 const [open,setOpen] = useState(false);
+
+const handleDrawerOpen = () => {
+  setOpen(true);
+}
+
+const handleDrawerClose = () => {
+  setOpen(false);
+}
   return (
+    <Box sx={{display:'flex'}}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton 
+          color='inherit'
+          aria-label='open drawer'
+          onClick={handleDrawerOpen}
+          edge='start'
+          sx={{marginRight: 5, ...(open && { display: 'none'}),
+        }}
+          >
+           <MenuIcon/>
+          </IconButton>
+          <Typography variant='h6' noWrap component='div'>
+            Transportes Pegaso
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+      sx={{
+        width: drawerWidth,
+        flexShrink:0,
+        '& .MuiDrawer-paper': {
+          width: drawerWidth,
+          boxSizing: 'border-box'
+        },
+      }}
+      variant='persistent'
+      anchor='left'
+      open={open}
+      >
+      <DrawerHeader>
+        <IconButton onClick={handleDrawerClose}>
+          {theme.direction === 'ltr' ? <ChevronLeftIcon/> :<ChevronRightIcon/> } 
+        </IconButton>
+      </DrawerHeader>
+      <Divider/>
+      </Drawer>
     <>
-    
+
       <MapContainer center={[51.505, -0.09]} zoom={13} >
        <TileLayer
        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -92,6 +145,7 @@ const [open,setOpen] = useState(false);
        />
     </MapContainer>
     </>
+    </Box>
   )
 }
 
