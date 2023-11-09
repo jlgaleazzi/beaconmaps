509f1f580d35345d222552193ba6cca6b1ab0fe8
Juan Galeazzi <jgaleazzi@gmail.com>
2023-11-08 21:46:48 -0600

    added left menu

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,8 @@ import MenuIcon from '@mui/icons-material/Menu'
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import { MapContainer, TileLayer, Marker, Popup, ZoomControl } from "react-leaflet";
-import { CssBaseline, IconButton, Toolbar, Typography } from "@mui/material";
+import { CssBaseline, IconButton, ThemeProvider, Toolbar, Typography, createTheme } from "@mui/material";
+import MenuList from "./components/menuList";
 
 const drawerWidth = 240;
 
@@ -70,7 +71,13 @@ const AppBar = styled(MuiAppBar , {
 function App() {
 
 const theme = useTheme();
+const dark = createTheme({
+  palette: {
+    mode: 'dark'
+  }
+})
 const [open,setOpen] = useState(false);
+
 
 const handleDrawerOpen = () => {
   setOpen(true);
@@ -80,55 +87,58 @@ const handleDrawerClose = () => {
   setOpen(false);
 }
   return (
-    <Box sx={{display:'flex'}}>
-      <CssBaseline />
-      <AppBar position="fixed" open={open}>
-        <Toolbar>
-          <IconButton
-          color='inherit'
-          aria-label='open drawer'
-          onClick={handleDrawerOpen}
-          edge='start'
-          sx={{marginRight: 5, ...(open && { display: 'none'}),
-        }}
-          >
-           <MenuIcon/>
-          </IconButton>
-          <Typography variant='h6' noWrap component='div'>
-            Transportes Pegaso
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-      sx={{
-        width: drawerWidth,
-        flexShrink:0,
-        '& .MuiDrawer-paper': {
+    <ThemeProvider theme={dark}>
+      <Box sx={{display:'flex'}}>
+        <CssBaseline />
+        <AppBar position="fixed" open={open}>
+          <Toolbar>
+            <IconButton
+            color='inherit'
+            aria-label='open drawer'
+            onClick={handleDrawerOpen}
+            edge='start'
+            sx={{marginRight: 5, ...(open && { display: 'none'}),
+          }}
+            >
+            <MenuIcon/>
+            </IconButton>
+            <Typography variant='h6' noWrap component='div'>
+              Transportes Pegaso
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+        sx={{
           width: drawerWidth,
-          boxSizing: 'border-box'
-        },
-      }}
-      variant='persistent'
-      anchor='left'
-      open={open}
-      >
-      <DrawerHeader>
-        <IconButton onClick={handleDrawerClose}>
-          {theme.direction === 'ltr' ? <ChevronLeftIcon/> :<ChevronRightIcon/> }
-        </IconButton>
-      </DrawerHeader>
-      <Divider/>
-      </Drawer>
-    <Main open={open}>
-      <MapContainer center={[19.432, -99.133]} zoom={11} zoomControl={false} >
+          flexShrink:0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box'
+          },
+        }}
+        variant='persistent'
+        anchor='left'
+        open={open}
+        >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon/> :<ChevronRightIcon/> }
+          </IconButton>
+        </DrawerHeader>
+        <Divider/>
+        <MenuList />
+        </Drawer>
+      <Main open={open}>
+        <MapContainer center={[19.432, -99.133]} zoom={11} zoomControl={false} >
 
-       <TileLayer
-       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-       />
-    </MapContainer>
-    </Main>
-    </Box>
+        <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+      </Main>
+      </Box>
+    </ThemeProvider>
   )
 }
 
---
+++ src/components/menuList.tsx
@@ -0,0 +1,81 @@
+
+import {List ,ListItemButton, ListItemIcon, ListItem, ListItemText, Collapse, Checkbox} from '@mui/material';
+import LayersIcon from '@mui/icons-material/Layers';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+import { useState } from 'react';
+
+
+const MenuList = () => {
+    const [checked,setChecked] = useState([1])
+    const [open, setOpen] = useState(false)
+    const handleClick = () => {
+        setOpen(!open)
+    }
+
+    const handleToggle = (value:number) => () => {
+        const currentIndex = checked.indexOf(value);
+        const newChecked = [...checked];
+
+        if (currentIndex === -1) {
+            newChecked.push(value)
+        } else {
+            newChecked.splice(currentIndex, 1)
+        }
+
+        setChecked(newChecked);
+    }
+
+    return (
+        <List 
+        sx={{width:'100%', maxWidth:'240px'}}
+        component='nav'
+        aria-labelledby='left-menu' 
+        >
+        <ListItemButton onClick={handleClick}>
+            <ListItemIcon>
+                <LayersIcon/>
+            </ListItemIcon>
+            <ListItemText primary ='Layers' />
+            {open ? <ExpandLess /> : <ExpandMore/>}
+        </ListItemButton>    
+        <Collapse in={open} timeout='auto' unmountOnExit>
+            <List component='div' disablePadding>
+                <ListItem key='units'
+                secondaryAction={
+                    <Checkbox 
+                    edge='end'
+                    onChange={handleToggle(1)}
+                    checked={checked.indexOf(1) !== -1}
+                     />
+                }
+                disablePadding
+                >
+                    <ListItemButton sx={{pl:4}}>
+                        <ListItemText primary='Unidades' />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem key='units'
+                secondaryAction={
+                    <Checkbox 
+                    edge='end'
+                    onChange={handleToggle(2)}
+                    checked={checked.indexOf(2) !== -1}
+                     />
+                }
+                disablePadding
+                >
+                    <ListItemButton sx={{pl:4}}>
+                        <ListItemText primary='Almacenes' />
+                    </ListItemButton>
+                </ListItem>
+
+              
+            </List>
+
+        </Collapse>
+        </List>
+    )
+}
+
+export default MenuList
