940a530dab806510a9f255f1759c0d7cbcf0ae33
Juan Galeazzi <jgaleazzi@gmail.com>
2024-01-14 20:13:01 -0600

    added put on server

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const Path = require('path');
 const port = 3001;
 const app = express();
 const fs = require('fs');
+const bodyParser = require('body-parser');
 
 app.use((_,res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
@@ -27,6 +28,27 @@ app.get("/units", (req , res) => {
         res.json(jsonData);
             })
         });
+
+app.put("/updateunits", (req, res) => {
+    fs.readFile(`${__dirname}/data/data.json`, 'UTF8', (err,data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send(`Internal Server Error ${JSON.stringify(__dirname)}`);
+            return;
+        }
+        let jsonData = JSON.parse(data);
+        console.log('updating units');
+        jsonData = req.body;
+        const updatedJson = JSON.stringify()
+        fs.writeFile(`${__dirname}/data/data.json`, updatedJson, 'UTF8', (err) => {
+            if (err) {
+                console.error('Error writing the file', err)
+            } else {
+                console.log('Units updated successfully!')
+            }
+        })
+    });
+})
 
 
 
