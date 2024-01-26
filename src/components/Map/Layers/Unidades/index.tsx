aa9d3670eb26ef74df53041792976c29f29abdd6
Juan Galeazzi <jgaleazzi@gmail.com>
2024-01-26 11:31:52 -0600

    added to self units

--- src/components/Map/Layers/Unidades/index.tsx
+++ src/components/Map/Layers/Unidades/index.tsx
@@ -1,4 +1,42 @@
+import { useState } from "react";
+import IUnit from "../Iunits";
+
 const Unidades = (map:tt.Map) => {
+
+interface Marker {
+  id: string | undefined;
+  obj: tt.Marker;
+}
+
+const [markers,setMarkers] = useState<Marker[]>([]);
+
+  const popupHtml = (unit:IUnit, label?:string):string => {
+    const html = label === 'Unidades' ?
+    `<div class='popUp'>
+      id: ${unit.identifier} <br/>
+      origen: ${unit.origin} <br/>
+      destino: ${unit.destination} <br/>
+      </div>` :  `<div class='popUp'>
+      Almacen
+    </div>`
+    ;
+    return html
+  }
+
+  const removeMarkers = () => {
+    markers.forEach((m) => {
+         console.log(`'removing marker' ${m.id}`)
+          m.obj.remove();
+     })
+     setMarkers([]);
+ }
+
+    const addMarkers = () => {
+
+    }
+
+
+
 return null;
 }
 
